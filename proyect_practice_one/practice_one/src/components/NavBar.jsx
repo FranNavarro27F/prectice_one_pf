51e166ea97ef3489ae6d74186919c266277e8d2a
Ramiro <ramiroferradas@hotmail.com>
2022-09-07 12:34:17 -0300

    aca rompiendo to2

--- proyect_practice_one/practice_one/src/components/NavBar.jsx
+++ proyect_practice_one/practice_one/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'react-dom'
+import React from "react";
+import Link from "react-dom";
 export default function NavBar() {
   return (
     <div>
@@ -8,9 +8,117 @@ export default function NavBar() {
           <li>1</li>
           <li>2</li>
           <li>3</li>
-
+          <h1>HOLA FRAN MIRA COMO ROMPO TODO AJAJAJAJAJ</h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis illo,
+          cumque corrupti accusantium eius at beatae accusamus fugiat a ducimus
+          sunt? Sequi cum, iure quis vero fuga accusantium facere incidunt!
         </ul>
       </nav>
-     </div>
-  )
+    </div>
+  );
 }
